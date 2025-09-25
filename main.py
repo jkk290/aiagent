d3e81880c2448d474338a4e9f7e4d19a554c463e
jkk290 <jared@kuromoto.dev>
2025-09-24 14:36:25 -1000

    Add --verbose to view prompt/response info

--- main.py
+++ main.py
@@ -2,25 +2,34 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
-def aiagent(prompt):
+def aiagent(args):
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
 
+    prompt = args[1]
     client = genai.Client(api_key=api_key)
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=prompt)]),
+    ]
    
     response = client.models.generate_content(
-        model="gemini-2.0-flash-001", contents=prompt)
-
+        model="gemini-2.0-flash-001", contents=messages)   
+    
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    if "--verbose" in args:   
+        print(f"User prompt: {prompt}")     
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 def main():
     if len(sys.argv) < 2:
-        raise Exception("must enter a prompt")
+        raise Exception("must enter a prompt, eg. python3 main.py \"why was 6 afraid of 7?\" [--verbose]")
         sys.exit(1)
-    aiagent(sys.argv[1])
+    aiagent(sys.argv)
 
 if __name__ == "__main__":
     main()

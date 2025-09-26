aa8e7ebecdc0d8df5977bef87a09f94dccced59e
jkk290 <jared@kuromoto.dev>
2025-09-25 14:24:46 -1000

    Add get_file_info tool to ai agent

--- main.py
+++ main.py
@@ -3,10 +3,25 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from functions.get_files_info import schema_get_files_info
 
 def aiagent(args):
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
+    system_prompt = """
+        You are a helpful AI coding agent.
+
+        When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+        - List files and directories
+
+        All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+        """
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+        ]
+    )
 
     prompt = args[1]
     client = genai.Client(api_key=api_key)
@@ -16,9 +31,14 @@ def aiagent(args):
     ]
    
     response = client.models.generate_content(
-        model="gemini-2.0-flash-001", contents=messages)   
-    
-    print(response.text)
+        model="gemini-2.0-flash-001", contents=messages, config=types.GenerateContentConfig(
+        tools=[available_functions], system_instruction=system_prompt
+    ),)
+
+    if not response.function_calls == None:
+        print(f"Calling function: {response.function_calls[0].name} ({response.function_calls[0].args})")
+    if response.function_calls == None:
+        print(response.text)
 
     if "--verbose" in args:   
         print(f"User prompt: {prompt}")     
